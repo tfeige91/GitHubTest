ae758759ac9a44f361e5db06dbbae593fea5d8ce
Timfeige <feige_tim@yahoo.de>
2024-10-21 13:33:25 +0200

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Tim_Feige on 21.10.24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
